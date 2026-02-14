8a704073cf3d866f8383174af627828eeac85ed2
Juan Manuel Anton <juanma.03.17.07@gmail.com>
2026-02-13 23:38:45 -0500

    BaseTable support objects

--- src/components/Common/BaseTable.vue
+++ src/components/Common/BaseTable.vue
@@ -3,6 +3,13 @@ defineProps({
     headers: { type: Array, required: true }, // [{title: 'ID', key: 'id'}, ...]
     items: { type: Array, required: true }
 });
+
+// Función para resolver claves anidadas como "businessLine.name"
+const getValue = (item, key) => {
+    if (!key) return null;
+    // Divide la cadena por el punto y navega por el objeto
+    return key.split('.').reduce((obj, i) => (obj ? obj[i] : null), item);
+};
 </script>
 
 <template>
@@ -18,7 +25,7 @@ defineProps({
             <tr v-for="item in items" :key="item.id">
                 <td v-for="header in headers" :key="header.key">
                     <slot :name="header.key" :item="item">
-                        {{ item[header.key] }}
+                        {{ getValue(item, header.key) }}
                     </slot>
                 </td>
             </tr>
